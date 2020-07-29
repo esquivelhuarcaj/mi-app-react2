55da32517a34b3e424e06337a3c338b1a35f995b
Jose Esquivel Huarca <esquivelhuarcaj@gmail.com>
2020-07-29 18:30:04 -0500

    Updates

---
+++ precache-manifest.b520d329c8f9107f2ff84c9795564080.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd9a37b09f49de74e1c3f26a2ef3510",
+    "url": "/my-app-react3/index.html"
+  },
+  {
+    "revision": "5d6db2c4302e61eba794",
+    "url": "/my-app-react3/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f97528078f08d8689be1",
+    "url": "/my-app-react3/static/js/2.2ebf5461.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app-react3/static/js/2.2ebf5461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d6db2c4302e61eba794",
+    "url": "/my-app-react3/static/js/main.564a79bd.chunk.js"
+  },
+  {
+    "revision": "0dfdea1b41f0fbae99c6",
+    "url": "/my-app-react3/static/js/runtime-main.71182fe0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app-react3/static/media/logo.5d5d9eef.svg"
+  }
+]);
